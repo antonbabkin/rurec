f909c0194a9a37e5939770a54b4fd26bfa5609de
Anton Babkin <anton.babkin@uconn.edu>
2020-12-22 14:51:42 -0600

    Add UI to run rurality

--- rurec/ui.py
+++ rurec/ui.py
@@ -6,9 +6,13 @@ __all__ = []
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Perform tasks within Rural Economy project.')
-    parser.add_argument('task', help='choose a task', choices=['build_parquet'])
+    parser.add_argument('task', help='choose a task', choices=['build_parquet', 'add_rurality'])
     args = parser.parse_args()
+    print(args.task, 'started')
     if args.task == 'build_parquet':
         from rurec import infogroup
         infogroup.build_parquet_dataset(21)
-        print(args.task, 'finished')
+    elif args.task == 'add_rurality':
+        from rurec import rurality
+        rurality.build_parquet_dataset(11)
+    print(args.task, 'finished')
