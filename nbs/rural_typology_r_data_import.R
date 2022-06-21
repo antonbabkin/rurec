7022d05f8dbd59d6ac2a9831514215ae688790ff
Austin Sandler <86606720+amsandler@users.noreply.github.com>
2022-06-21 08:35:51 -0400

    Update rural_typology_r_data_import.R

--- nbs/rural_typology_r_data_import.R
+++ nbs/rural_typology_r_data_import.R
@@ -1,71 +1,103 @@
-root_dir <- rprojroot::find_rstudio_root_file()
+# Import all downloaded outside data
+
+# Load and attach necessary packages
+library(rprojroot)
+library(magrittr)
+library(sf)
 
 # Connect and parse code from another file 
-source(file.path(root_dir, "nbs", "rural_typology_r_data_sources.R"))
+source(file.path(find_rstudio_root_file(), "nbs", "rural_typology_r_data_sources.R"))
+
+# Create r object only data folder if not already done
+data_dir = file.path(find_rstudio_root_file(), "data", "robjs")
+if (!file.exists(data_dir)) {
+  dir.create(data_dir)
+}
 
 
 # Import IO tables 
-excel_importr(
-  TableName = "IO_tables",
-  FileDir = file.path(root_dir, "data", "AllTablesSUP") 
-)
-
-
-# Import CBP data 
-if (!exists("CBP_2019")){
-  CBP_2019 <- file.path(root_dir, "data", "cbp19co.txt") %>% read.csv(header = TRUE)
-  
-  CBP_2019$fipstate %<>% formatC(width = 2, format = "d", flag = "0")
-  CBP_2019$fipscty  %<>% formatC(width = 3, format = "d", flag = "0")
-  CBP_2019$place <- paste0(CBP_2019$fipstate, CBP_2019$fipscty)
+if (!file.exists(file.path(data_dir, "IO_tables"))){
+  excel_importr(
+    TableName = "IO_tables",
+    FileDir = file.path(find_rstudio_root_file(), "data", "AllTablesSUP") 
+  )
 }
+saver(IO_tables)
+
+
+# Import CBP data 2019
+if (!file.exists(file.path(data_dir, "CBP_2019"))){
+  if (!exists("CBP_2019")){
+    CBP_2019 <- file.path(find_rstudio_root_file(), "data", "cbp19co.txt") %>% read.csv(header = TRUE)
+    
+    CBP_2019$fipstate %<>% formatC(width = 2, format = "d", flag = "0")
+    CBP_2019$fipscty  %<>% formatC(width = 3, format = "d", flag = "0")
+    CBP_2019$place <- paste0(CBP_2019$fipstate, CBP_2019$fipscty)
+  }
+}
+saver(CBP_2019)
 
 
 # Import CBP data 2012 
-if (!exists("CBP_2012")){
-  CBP_2012 <- file.path(root_dir, "data", "cbp12co.txt") %>% read.csv(header = TRUE)
-  
-  CBP_2012$fipstate %<>% formatC(width = 2, format = "d", flag = "0")
-  CBP_2012$fipscty  %<>% formatC(width = 3, format = "d", flag = "0")
-  CBP_2012$place <- paste0(CBP_2012$fipstate, CBP_2012$fipscty)
+if (!file.exists(file.path(data_dir, "CBP_2012"))){
+  if (!exists("CBP_2012")){
+    CBP_2012 <- file.path(find_rstudio_root_file(), "data", "cbp12co.txt") %>% read.csv(header = TRUE)
+    
+    CBP_2012$fipstate %<>% formatC(width = 2, format = "d", flag = "0")
+    CBP_2012$fipscty  %<>% formatC(width = 3, format = "d", flag = "0")
+    CBP_2012$place <- paste0(CBP_2012$fipstate, CBP_2012$fipscty)
+  }
 }
-
+saver(CBP_2012)
 
 #Import 2020 BLS Quarterly Census of Employment and Wages (QCEW) 
-if (!exists("QCEW_2020")){
-  QCEW_2020 <- file.path(root_dir, "data", "2020.annual.singlefile.csv") %>% read.csv(header = TRUE)
+if (!file.exists(file.path(data_dir, "QCEW_2020"))){
+  if (!exists("QCEW_2020")){
+    QCEW_2020 <- file.path(find_rstudio_root_file(), "data", "2020.annual.singlefile.csv") %>% read.csv(header = TRUE)
+  }
 }
+saver(QCEW_2020)
 
 # Import TIGER data 
-if (!exists("TIGERData")){
-  TIGERData <- file.path(root_dir, "data", "cb_2021_us_county_500k.shp") %>% st_read(stringsAsFactors = FALSE)
-  TIGERData$place <- paste0(TIGERData$STATEFP, TIGERData$COUNTYFP)
-  TIGERData$center <- st_centroid(TIGERData$geometry)
-  TIGERData %<>% arrange(place)
+if (!file.exists(file.path(data_dir, "TIGERData"))){
+  if (!exists("TIGERData")){
+    TIGERData <- file.path(find_rstudio_root_file(), "data", "cb_2021_us_county_500k.shp") %>% st_read(stringsAsFactors = FALSE)
+    TIGERData$place <- paste0(TIGERData$STATEFP, TIGERData$COUNTYFP)
+    TIGERData$center <- st_centroid(TIGERData$geometry)
+    TIGERData %<>% arrange(place)
+  }
 }
-
+saver(TIGERData)
 
 # Import RUCA tables 
-if (!exists("RUCAData")){
-  RUCAData <- file.path(root_dir, "data", "ruca2010revised.xlsx") %>% read.xlsx(startRow = 2)
+if (!file.exists(file.path(data_dir, "RUCAData"))){
+  if (!exists("RUCAData")){
+    RUCAData <- file.path(find_rstudio_root_file(), "data", "ruca2010revised.xlsx") %>% read.xlsx(startRow = 2)
+  }
 }
+saver(RUCAData)
 
 # Import RUCC tables 
-if (!exists("RUCCData")){
-  RUCCData <- file.path(root_dir, "data", "ruralurbancodes2013.xls") %>% read_xls()
-}
+if (!file.exists(file.path(data_dir, "RUCCData"))){
+  if (!exists("RUCCData")){
+    RUCCData <- file.path(find_rstudio_root_file(), "data", "ruralurbancodes2013.xls") %>% read_xls()
+  }
 RUCCData$place <- RUCCData$FIPS
+}
 ####  Note:  2 counties (02158, 46102) are not common to TIGER_CBP in RUCCData
-
+saver(RUCCData)
 
 # Import UIC tables 
-if (!exists("UICData")){
-  UICData <- file.path(root_dir, "data", "UrbanInfluenceCodes2013.xls") %>% read_xls()
-}
+if (!file.exists(file.path(data_dir, "UICData"))){
+  if (!exists("UICData")){
+    UICData <- file.path(find_rstudio_root_file(), "data", "UrbanInfluenceCodes2013.xls") %>% read_xls()
+  }
 UICData$place <- UICData$FIPS
+}
+saver(UICData)
 
-
-
+# Remove clutter
+rm(CBP_2012, CBP_2019, IO_tables, QCEW_2020, TIGERData, RUCAData, RUCCData, UICData, data_dir)
 
 
 
