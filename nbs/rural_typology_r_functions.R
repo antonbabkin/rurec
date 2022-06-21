dd329229379e99800e70febf7a3cad829e5c0013
Austin Sandler <86606720+amsandler@users.noreply.github.com>
2022-06-21 08:35:10 -0400

    Update rural_typology_r_functions.R

--- nbs/rural_typology_r_functions.R
+++ nbs/rural_typology_r_functions.R
@@ -1,29 +1,24 @@
-# List packages needed for this exercise
-packages <- c("dlm",
-              "geosphere",
-              "gtools",
-              "knitr",
-              "magrittr",
-              "Matrix",
-              "openxlsx",
-              "RColorBrewer",
-              "readxl",
-              "reticulate",
-              "sf",
-              "spdep",
-              "tidyverse",
-              "tmap",
-              "tmaptools",
-              "tools")
+# Function repository 
 
-# Load packages as necessary
-invisible(lapply(packages, library, character.only = TRUE))
+# Load and attach necessary packages
+library(rprojroot)
+library(fs)
+library(readxl)
+library(openxlsx)
 
-
-root_dir <- rprojroot::find_rstudio_root_file()
-
-# Connect and parse code from another file 
-source(file.path(root_dir, "nbs", "rural_typology_r_packages_all.R"))
+# #Function to save r functions
+# fun_saver <- function (funct){
+#   
+#   # Create r function folder if not already done
+#   fun_dir = file.path(find_rstudio_root_file(), "data", "rfuns")
+#   if (!file.exists(fun_dir)) {
+#     dir.create(fun_dir)
+#   }
+#   
+#   if (!file.exists(file.path(fun_dir, as.character(substitute(funct))))){
+#     saveRDS(funct, file = file.path(fun_dir, as.character(substitute(funct))))
+#   }
+# }
 
 
 # Function to download  data
@@ -43,14 +38,22 @@ data_zipr <- function(ZipURL,
                       DestDir,
                       FileExt = ""){
   local({
-    DestFile <- ZipURL %>% basename() %>% file_path_sans_ext() %>% path(DestDir, ., ext = FileExt)
-    temp <- tempfile()
-    if (!file.exists(DestFile)) {
-      download.file(url=ZipURL, destfile=temp, quiet=TRUE)
-      if (file.info(temp)$size > 0){
-        unzip(zipfile = temp, exdir = DestDir, overwrite = FALSE)
-      }
+    
+    zip_dir = file.path(DestDir, "rawzip")
+    if (!file.exists(zip_dir)) {
+      dir.create(zip_dir)
     }
+    
+    DestFile <- ZipURL %>% basename() %>% file_path_sans_ext() %>% path(DestDir, ., ext = FileExt)
+    ZipDest <- ZipURL %>% basename() %>% file_path_sans_ext() %>% file.path(zip_dir,  .)
+    if (!file.exists(ZipDest)) {
+      download.file(url=ZipURL, destfile=ZipDest, quiet=TRUE)
+    }
+      if (!file.exists(DestFile)) {
+        if (file.info(ZipDest)$size > 0){
+          unzip(zipfile = ZipDest, exdir = DestDir, overwrite = FALSE)
+        }
+      }
   })
 }
 
@@ -86,6 +89,7 @@ finiter <- function(x){
   }
 } 
 
+
 finiterer <- function(x){ 
   lapply(1:length(x), function(i) apply(x[[i]], c(1,2), finiter))
 }
@@ -100,14 +104,33 @@ oner <- function(x, t=1, l=1){
     x=x
   }
 }
+
+
 onerer <- function(x){ 
   lapply(1:length(x), function(i) apply(x[[i]], c(1,2), oner))
 }
 
 
+#Function to save parsed r data tables
+saver <- function (dataname){
+  
+  data_dir = file.path(find_rstudio_root_file(), "data", "robjs")
+  if (!file.exists(data_dir)) {
+    dir.create(data_dir)
+  }
+  
+  if (!file.exists(file.path(data_dir, as.character(substitute(dataname))))){
+    saveRDS(dataname, file = file.path(data_dir, as.character(substitute(dataname))))
+  }
+
+}
 
 
-
+#Function to import robj data
+importr <- function(x){
+  require(rprojroot)
+  assign(deparse(substitute(x)), readRDS(file.path(find_rstudio_root_file(), "data", "robjs", as.character(substitute(x)) )), envir=.GlobalEnv)
+}
 
 
 
