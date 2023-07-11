0e3369062e8d84b14e824638269a29f934ca68eb
Austin Sandler <86606720+amsandler@users.noreply.github.com>
2023-07-11 08:33:33 -0400

    Update io_analysis.R

--- nbs/io_analysis.R
+++ nbs/io_analysis.R
@@ -14,7 +14,7 @@
 #' 
 #' @examples
 #' ras_trade_flows(matrix(1, 3, 3), c(0,2,3), c(3,0,2))
-ras_trade_flows <- function (x0, rs1, cs1, tol = 1e-2, maxiter = 1000, verbose = FALSE) 
+ras_trade_flows <- function (x0, rs1, cs1, tol = 1e-3, maxiter = 1000, verbose = FALSE) 
 {
   # test if targets sum to same total, within 0.1% tolerance
   sum_tol <- 0.001
@@ -22,27 +22,45 @@ ras_trade_flows <- function (x0, rs1, cs1, tol = 1e-2, maxiter = 1000, verbose =
   if (sum_dif > sum_tol) stop("sum(rs1) != sum(cs1)")
 
   # mask away all-zero rows and columns
-  rpos <- (rs1 > 0) & (rowSums(x0) > 0)
-  cpos <- (cs1 > 0) & (colSums(x0) > 0)
+  # rpos <- (rs1 > 0) & (rowSums(x0) > 0)
+  # cpos <- (cs1 > 0) & (colSums(x0) > 0)
+  # rpos <- (rs1 > 0) & (rowSums(x0[,cs1 > 0]) != 0)
+  # cpos <- (cs1 > 0) & (colSums(x0[rs1 > 0,]) != 0)
+  rpos <- (rs1 > 0) & sapply(rowSums(x0[,cs1 > 0, drop=F]), function(x){!isTRUE(all.equal(x, 0))} )
+  cpos <- (cs1 > 0) & sapply(colSums(x0[rs1 > 0, ,drop=F]), function(x){!isTRUE(all.equal(x, 0))} )
   x <- x0[rpos, cpos, drop=F]
   nr <- nrow(x)
   nc <- ncol(x)
   rs <- rs1[rpos]
   cs <- cs1[cpos]
-  
+  mad <- -1
   for (i in 1:maxiter) {
     # scale rows
     x1 <- matrix(rs / rowSums(x), nr, nc) * x
     # scale cols
     x1 <- matrix(cs / colSums(x1), nr, nc, byrow = TRUE) * x1
-    rmse <- sqrt(mean((x1 - x)^2))
+    #rmse <- sqrt(mean((x1 - x)^2))
     #rmse <- max(abs(rowSums(x1) - rs) + abs(colSums(x1) - cs))
+    rmse <- max(max(abs(rowSums(x1) - rs)) , max(abs(colSums(x1) - cs)))
+    # tbr <- sum(x1) / sum(cs)
+    # eid <- sqrt(mean(((rowSums(x1) / rs) - 1)^2))
+    # iid <- sqrt(mean(((colSums(x1) / cs) - 1)^2))
+    # sts <- mean(diag(x1) / rowSums(x1))
+    # mad <- mean(abs(colSums(x1) - cs))
+    
+    #isTRUE(all.equal(mad, mean(abs(rowSums(x1) - rs))))
+
+    if (mad == mean(abs(rowSums(x1) - rs))) {warning("\n\n  No convergence: infeasible\n")
+      break}
+    mad <- mean(abs(rowSums(x1) - rs))
     x <- x1
-    if (verbose) cat(paste("  Iteration:", i, "  RMSE:", rmse, "\n"))
+    if (verbose) cat(paste("  Iteration:", i, "  RMSE:", rmse, " MAD:", mad, "\n"))
+    #if (verbose) cat(paste("  Iteration:", i, "  RMSE:", rmse, " MAD:", mad, " TBR:", tbr, " EID:", eid, " IID:", iid, " STS:", sts, "\n"))
     if (rmse < tol) break
   }
   if (i == maxiter) warning("\n\n  No convergence. Maximum Number of iterations reached. Consider increasing the number of iterations.\n")
   if (verbose) cat(paste("Number of iterations:", i, "RMSE:", rmse, "\n"))
+  
   
   # return zero rows and cols back
   xz <- matrix(0, nrow(x0), ncol(x0))
