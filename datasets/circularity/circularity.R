7b6f2791d0c8751b44d62f6af200672a1323ab49
Anton Babkin <6345499+antonbabkin@users.noreply.github.com>
2024-02-23 14:24:10 -0600

    Compile circularity dataset, start codebook with variable definitions

---
+++ datasets/circularity/circularity.R
@@ -0,0 +1,54 @@
+# This script compiles the circularity dataset
+
+
+# libraries and sources ----
+
+library(tidyverse)
+library(logger)
+log_threshold(DEBUG)
+
+source("R/basic_utilities.R", local = (util <- new.env()))
+source("R/circularity.R", local = (circularity_source <- new.env()))
+
+# paths ----
+
+# many inputs are required indirectly through sourced scripts
+ipath <- list(
+  
+)
+
+opath <- list(
+  circularity = "datasets/circularity/circularity.rds"
+)
+
+# dataset ----
+
+get_circularity <- function() {
+  cache_path = opath$circularity
+  if (file.exists(cache_path)) {
+    df <- readRDS(cache_path)
+    log_debug("read from cache {cache_path}")
+  } else {
+    df <- list()
+    for (year in c(2007, 2012, 2017)) {
+      df[[length(df) + 1]] <- circularity_source$call_circularity_metrics(
+        year = year,
+        class_system = "commodity",
+        paradigm = "domestic",
+        bus_data = "infogroup",
+        spatial = FALSE
+      ) |>
+        select(
+          place, gross_output, intermediate_supply, intermediate_demand, net_supply, 
+          net_demand, production_capacity, trade_capacity, retention, 
+          production_dependency, trade_dependency, autonomy, trade_balance, trade_openness) |>
+        rename(county_fips = place, excess_supply = net_supply, excess_demand = net_demand) |>
+        mutate(year = year, .before = 1)
+    }
+    df <- bind_rows(df)
+    
+    saveRDS(df, util$mkdir(cache_path))
+    log_debug("save to cache {cache_path}")
+  }    
+  df
+}
