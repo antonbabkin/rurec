4bd69b135578ab447b182f695b7bf0aaaf7d2417
Austin Sandler <86606720+amsandler@users.noreply.github.com>
2024-06-14 13:09:28 -0400

    Add imputed cpb to circular zip

--- datasets/circularity/circularity.R
+++ datasets/circularity/circularity.R
@@ -30,6 +30,7 @@ get_circularity <- function() {
     log_debug("read from cache {cache_path}")
   } else {
     df <- list()
+    for (d in c("infogroup", "cbp_imp"))
     for (l in c("det", "sum", "sec")){
       for (year in c(2007, 2012, 2017)) {
         df[[length(df) + 1]] <- circularity_source$call_circularity_metrics(
@@ -37,7 +38,7 @@ get_circularity <- function() {
           ilevel = l,
           class_system = "commodity",
           paradigm = "domestic",
-          bus_data = "infogroup",
+          bus_data = d,
           spatial = FALSE
         ) |>
           select(
@@ -45,6 +46,7 @@ get_circularity <- function() {
             net_demand, production_capacity, trade_capacity, retention, 
             production_dependency, trade_dependency, autonomy, trade_balance, trade_openness) |>
           rename(excess_supply = net_supply, excess_demand = net_demand) |>
+          mutate(bus_data = d, .before = 1) |>
           mutate(ilevel = l, .before = 1) |>
           mutate(year = year, .before = 1)
       }
